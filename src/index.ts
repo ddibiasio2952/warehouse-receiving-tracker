303e3019de97def86c2e4a54699e855e1c52a74b
ddibiasio2952 <ddibiasio_2952@email.ric.edu>
2026-08-26 21:14:08 -0400

    Initialize Node.js and TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Declare a variable named projectName with an explicit string type.
+// Log that variable to the console.
+
+let projectName: string = "receiving-discrepancy-tracker";
+console.log(projectName);
+
+/*
+function logProject(projectName: string): void {
+    console.log("Initializing ", projectName.toUpperCase());
+}
+
+logProject(projectName);
+*/
